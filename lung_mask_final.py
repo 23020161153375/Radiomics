6899bc1e371ff622b96e88c4bafa60421f9bc971
zhwang <zhwang@ift-informatik.de>
2020-12-08 21:55:33 +0100

    lung_mask_final.py can extract lung from the image

--- lung_mask_final.py
+++ lung_mask_final.py
@@ -15,6 +15,7 @@ def fill_color_demo(image):
 
 def lung_mask(image):
     image_tmp = image.copy()
+    image_ori= image.copy()
 
     # Step2. 二值化
     ret, thresh = cv2.threshold(image_tmp, 10, 255, cv2.THRESH_BINARY)
@@ -25,18 +26,18 @@ def lung_mask(image):
     opening = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)  # 闭运
     opening = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel)  # 开运算
     # Step3. 轮廓提取
-    #
-    # contour, hierarchy = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #
-    # # Step4. 轮廓绘制
-    # color = cv2.cvtColor(image_tmp, cv2.COLOR_GRAY2BGR)
-    # result = cv2.drawContours(color, contour, -1, (0, 255, 0), 2)  # 轮廓用绿色绘制
-    #
-    # copyIma = fill_color_demo(result)
+
+    contour, hierarchy = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Step4. 轮廓绘制
+    color = cv2.cvtColor(image_tmp, cv2.COLOR_GRAY2BGR)
+    result = cv2.drawContours(color, contour, -1, (0, 255, 0), 2)  # 轮廓用绿色绘制
+
+    copyIma = fill_color_demo(result)
 
     mask = image_tmp
-    # mask[copyIma[:, :, 0] == 100] = 1
-    # mask[copyIma[:, :, 0] != 100] = 0
+    mask[copyIma[:, :, 0] == 100] = 1
+    mask[copyIma[:, :, 0] != 100] = 0
 
     kernel = np.ones((3, 3), np.uint8)
     kernel2 = np.ones((10, 10), np.uint8)
@@ -45,13 +46,12 @@ def lung_mask(image):
     kernel5 = np.ones((16, 16), np.uint8)
     erosion = cv2.erode(mask, kernel, iterations=1)
     dilation = cv2.dilate(erosion, kernel5, iterations=1)
-    output_image = image * dilation
+    output_image = image_ori * dilation
     return output_image
 
 
 
-
 if __name__=='__main__':
-    image = cv2.imread('1.jpg')
-    result = lung_mask(image)
+    image_tmp = cv2.imread('1.jpg',0)
+    result = lung_mask(image_tmp)
     cv2.imwrite("result_one.jpg",result)
